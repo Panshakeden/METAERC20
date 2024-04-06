1db250d08866f215d6a87d822d2ab1ac03afc84f
panshak <panshakeden@gmail.com>
2024-04-06 12:55:35 +0100

    initail commit

---
+++ contracts/ERC20.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "./IERC20.sol";
+
+contract ERC20 is IERC20 {
+
+    string public tokenName;
+    string public symbol;
+    uint256 public totalSupply;
+    address public owner;
+    uint256  public decimal;
+
+    mapping(address => uint256) private balances;
+
+    constructor(
+       
+    ) {
+        tokenName ="MetaCave";
+        symbol = "CVE";
+        decimal = 18;
+        owner = msg.sender;
+    }
+
+    function onlyOwner() private view {
+        require(owner == msg.sender, "you are not the owner");
+    }
+
+    function mint(address _account, uint256 _amount) external {
+        onlyOwner();
+        require(_amount > 0);
+
+        totalSupply = totalSupply + _amount;
+        balances[_account] += _amount;
+    }
+
+    function transfer(address _to, uint256 _value) external returns (bool) {
+        require(msg.sender != address(0), "Address 0 not allowed");
+        require(totalSupply >= balances[msg.sender]);
+
+        uint256 Bal = balances[msg.sender];
+        require(Bal >= _value);
+
+        balances[msg.sender] = Bal - _value;
+        balances[_to]  += _value;
+
+        assert(balances[msg.sender] == (Bal - _value));
+
+        return true;
+    }
+
+    function burn(uint96 _amount) external {
+        require(msg.sender != address(0), "Address 0 not allowed");
+        require(balances[msg.sender] > 0);
+        require(balances[msg.sender] >= _amount);
+        
+        balances[msg.sender] -= _amount;
+        totalSupply -= _amount;
+
+        balances[address(0)] += _amount;
+    }
+
+    function balanceOf(address _owner) external view returns (uint256) {
+        return balances[_owner];
+    }
+
+
+    // 0x0000000000000000000000000000000000000000
+}
